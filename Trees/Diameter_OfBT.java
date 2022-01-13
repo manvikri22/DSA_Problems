d7bc8e8cdf5db99d1582d147107bfd9e7ff2d126
Manvikri22 <manvikri2@gmail.com>
2022-01-13 11:07:32 +0530

    init

---
+++ Trees/Diameter_OfBT.java
@@ -0,0 +1,47 @@
+package Trees;
+
+import java.util.*;
+public class Diameter_OfBT{
+    static Node root;
+    static int ans =0;
+    Diameter_OfBT(int d)
+    {
+        root = new Node(d);
+    }
+    Diameter_OfBT()
+    {
+        root = null;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Diameter_OfBT t1 = new Diameter_OfBT();
+        t1.root = new Node(1);
+        t1.root.left = new Node(2);
+        t1.root.right= new Node(3);
+        t1.root.left.right = new Node(4);
+
+        System.out.println("Diameter of the tree: "+Diameter(root));
+        sc.close();
+    }
+
+    public static int Diameter(Node root)
+    {
+        if(root ==null) return 0;
+        int lh = Diameter(root.left);
+        int rh = Diameter(root.right);
+        ans = Math.max(ans, 1+lh+rh);
+        return 1+ Math.max(lh, rh);
+    }
+
+}
+class Node
+{
+    int data;
+    Node left, right;
+
+    public Node(int d)
+    {
+        data = d;
+        left = right = null;
+    }
+}
