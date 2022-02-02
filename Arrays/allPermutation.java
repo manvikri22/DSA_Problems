ed74f82aa4f5f08074f31c65a80d6f069c9c0aa4
Manvikri22 <manvikri2@gmail.com>
2022-02-02 21:02:45 +0530

    commit

---
+++ Arrays/allPermutation.java
@@ -0,0 +1,15 @@
+package Arrays;
+import java.util.*;
+public class allPermutation {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter String: ");
+        String s = in.nextLine();
+        print(s);
+        in.close();
+    }
+    static void print(String s)
+    {
+
+    }
+}
