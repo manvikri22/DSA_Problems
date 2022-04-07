3cfb165f27816de34bf5ba603bf414f0a0b4bbad
Manvikri22 <manvikri2@gmail.com>
2022-04-07 13:09:28 +0530

    commit

--- CD/lex.yy.c
+++ CD/lex.yy.c
@@ -360,26 +360,26 @@ struct yy_trans_info
 	flex_int32_t yy_verify;
 	flex_int32_t yy_nxt;
 	};
-static const flex_int16_t yy_accept[11] =
+static const flex_int16_t yy_accept[10] =
     {   0,
-        2,    2,    5,    4,    2,    2,    2,    2,    1,    0
+        0,    0,    5,    4,    3,    2,    1,    1,    0
     } ;
 
 static const YY_CHAR yy_ec[256] =
     {   0,
+        1,    1,    1,    1,    1,    1,    1,    1,    2,    2,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
+        1,    2,    1,    1,    1,    1,    1,    1,    1,    3,
+        3,    3,    3,    1,    3,    1,    3,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
-        1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
-        1,    1,    1,    1,    1,    1,    1,    2,    2,    2,
-        2,    2,    2,    2,    2,    2,    2,    1,    1,    1,
-        1,    1,    1,    1,    3,    4,    4,    4,    3,    4,
-        4,    4,    3,    4,    4,    4,    4,    4,    3,    4,
-        4,    4,    4,    4,    3,    4,    4,    4,    4,    4,
-        1,    1,    1,    1,    1,    1,    3,    4,    4,    4,
+        1,    1,    1,    1,    4,    4,    4,    4,    4,    4,
+        4,    4,    4,    4,    4,    4,    4,    4,    4,    4,
+        4,    4,    4,    4,    4,    4,    4,    4,    4,    4,
+        1,    1,    1,    1,    1,    1,    4,    4,    4,    4,
 
-        3,    4,    4,    4,    3,    4,    4,    4,    4,    4,
-        3,    4,    4,    4,    4,    4,    3,    4,    4,    4,
+        4,    4,    4,    4,    4,    4,    4,    4,    4,    4,
+        4,    4,    4,    4,    4,    4,    4,    4,    4,    4,
         4,    4,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
@@ -399,31 +399,29 @@ static const YY_CHAR yy_ec[256] =
 
 static const YY_CHAR yy_meta[5] =
     {   0,
-        1,    2,    2,    2
+        1,    1,    1,    2
     } ;
 
-static const flex_int16_t yy_base[13] =
+static const flex_int16_t yy_base[11] =
     {   0,
-        0,    0,    7,    8,    0,    0,    0,    0,    0,    8,
-        4,    3
+        0,    0,    6,    7,    7,    7,    0,    0,    7,    3
     } ;
 
-static const flex_int16_t yy_def[13] =
+static const flex_int16_t yy_def[11] =
     {   0,
-       10,    1,   10,   10,   11,   12,   11,   11,   12,    0,
-       10,   10
+        9,    1,    9,    9,    9,    9,   10,   10,    0,    9
     } ;
 
-static const flex_int16_t yy_nxt[13] =
+static const flex_int16_t yy_nxt[12] =
     {   0,
-        4,    5,    6,    7,    9,    8,   10,    3,   10,   10,
-       10,   10
+        4,    5,    6,    7,    8,    9,    3,    9,    9,    9,
+        9
     } ;
 
-static const flex_int16_t yy_chk[13] =
+static const flex_int16_t yy_chk[12] =
     {   0,
-        1,    1,    1,    1,   12,   11,    3,   10,   10,   10,
-       10,   10
+        1,    1,    1,    1,   10,    3,    9,    9,    9,    9,
+        9
     } ;
 
 static yy_state_type yy_last_accepting_state;
@@ -440,12 +438,16 @@ int yy_flex_debug = 0;
 #define YY_MORE_ADJ 0
 #define YY_RESTORE_YY_MORE_OFFSET
 char *yytext;
-#line 1 "Lex1.l"
-#line 2 "Lex1.l"
-#include<stdio.h>
-int wc=0, vc=0, ch=0; 
+#line 1 "abc.l"
+#line 2 "abc.l"
+	/* Definition section */
+	#include <stdio.h>
+	#include <stdlib.h>
+	#include "y.tab.h"
 #line 448 "lex.yy.c"
-#line 449 "lex.yy.c"
+/* %option noyywrap */
+/* Rule Section */
+#line 451 "lex.yy.c"
 
 #define INITIAL 0
 
@@ -662,9 +664,10 @@ YY_DECL
 		}
 
 	{
-#line 6 "Lex1.l"
+#line 11 "abc.l"
 
-#line 668 "lex.yy.c"
+
+#line 671 "lex.yy.c"
 
 	while ( /*CONSTCOND*/1 )		/* loops until end-of-file is reached */
 		{
@@ -691,13 +694,13 @@ yy_match:
 			while ( yy_chk[yy_base[yy_current_state] + yy_c] != yy_current_state )
 				{
 				yy_current_state = (int) yy_def[yy_current_state];
-				if ( yy_current_state >= 11 )
+				if ( yy_current_state >= 10 )
 					yy_c = yy_meta[yy_c];
 				}
 			yy_current_state = yy_nxt[yy_base[yy_current_state] + yy_c];
 			++yy_cp;
 			}
-		while ( yy_base[yy_current_state] != 8 );
+		while ( yy_base[yy_current_state] != 7 );
 
 yy_find_action:
 		yy_act = yy_accept[yy_current_state];
@@ -723,25 +726,26 @@ do_action:	/* This label is used only to access EOF actions. */
 
 case 1:
 YY_RULE_SETUP
-#line 7 "Lex1.l"
-{vc++;} //vowel counter
+#line 13 "abc.l"
+{yylval.f = yytext; return STR;}
 	YY_BREAK
 case 2:
 YY_RULE_SETUP
-#line 8 "Lex1.l"
-{wc++;}  //word counter
+#line 14 "abc.l"
+{return yytext[0];}
 	YY_BREAK
 case 3:
+/* rule 3 can match eol */
 YY_RULE_SETUP
-#line 9 "Lex1.l"
-{ch++;} //characters counter
+#line 15 "abc.l"
+{;}
 	YY_BREAK
 case 4:
 YY_RULE_SETUP
-#line 10 "Lex1.l"
+#line 17 "abc.l"
 ECHO;
 	YY_BREAK
-#line 745 "lex.yy.c"
+#line 749 "lex.yy.c"
 case YY_STATE_EOF(INITIAL):
 	yyterminate();
 
@@ -1038,7 +1042,7 @@ static int yy_get_next_buffer (void)
 		while ( yy_chk[yy_base[yy_current_state] + yy_c] != yy_current_state )
 			{
 			yy_current_state = (int) yy_def[yy_current_state];
-			if ( yy_current_state >= 11 )
+			if ( yy_current_state >= 10 )
 				yy_c = yy_meta[yy_c];
 			}
 		yy_current_state = yy_nxt[yy_base[yy_current_state] + yy_c];
@@ -1066,11 +1070,11 @@ static int yy_get_next_buffer (void)
 	while ( yy_chk[yy_base[yy_current_state] + yy_c] != yy_current_state )
 		{
 		yy_current_state = (int) yy_def[yy_current_state];
-		if ( yy_current_state >= 11 )
+		if ( yy_current_state >= 10 )
 			yy_c = yy_meta[yy_c];
 		}
 	yy_current_state = yy_nxt[yy_base[yy_current_state] + yy_c];
-	yy_is_jam = (yy_current_state == 10);
+	yy_is_jam = (yy_current_state == 9);
 
 		return yy_is_jam ? 0 : yy_current_state;
 }
@@ -1746,25 +1750,10 @@ void yyfree (void * ptr )
 
 #define YYTABLES_NAME "yytables"
 
-#line 10 "Lex1.l"
+#line 17 "abc.l"
 
 
-int yywrap(){}
-int main()
+int yywrap()
 {
-// The function that starts the analysis
-yylex();
-
-printf("\nNo. of words=%d\n", wc);
-printf("\nNo. of vowels=%d\n", vc);
-printf("\nNo. of characters=%d\n", ch);
-
-return 0;
+return -1;
 }
-
-
-
-
-
-
-
